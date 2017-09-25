daeea32525ce1544be7607d6c6f1baddebc1cfe0
Patrick Taylor <taylor.patrick@charterschool.org>
2017-09-25 12:50:41 -0400

    add stacker with doc

--- Stack.java
+++ Stack.java
@@ -1,26 +1,39 @@
 
 /**
  * Write a description of class stack here.
- * 
+ * Makes a stack
  * @author Patrick Taylor
  * @version 1.0
  */
 public class Stack
 {
     Node top;
-    public void push(Node n){
-        
+    /**
+     * @param Node to go on top of stack
+     * @return nothing
+     * adds argument Node to top of stack
+     */
+    public void push(Node n){ 
         n.setNext(top);
         top = n;
-
     }
 
+    /**
+     * @param nothing
+     * @return top of stack
+     *removes top from stack and returns it 
+     */
     public Node pop(){
         Node popping = top;
         top = top.getNext();
         return popping;
     }
 
+    /**
+     * @param nothing
+     * @return top of stack
+     * see waht's on top 
+     */
     public Node peek(){
         return top;
     }
