721ff48c780738546ab55c0976f194fd000437dc
Patrick Taylor <taylor.patrick@charterschool.org>
2017-11-15 12:59:49 -0500

    finish print

---
+++ BST.java
@@ -0,0 +1,169 @@
+import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+/**
+ * BST
+ * Binary Search Tree class inteded to store data in a Binary Search Tree. The object in the generic
+ * class must be comparable (implements Comparable). For the prints to work, the object must be
+ * printable as well (implements toString()).
+ * 
+ * Seperate, recursive methods are used to implement recursive algorithms for operations.
+ * This is because most recursive methods require the parent to be a parameter, but
+ * generally when you call a method (like insert for example) the parent is assumed to
+ * be the root.
+ */
+public class BST <T> {
+    BSTNode <T> root;
+
+    /**
+     * insert a single element into the tree. It is potentially possible to mess up the tree by modifying an element after insertion.
+     * @param in the element to insert (the actual instance is inserted, NOT A CLONE)
+     */
+    public void insert(T in) {
+        if (in == null) return;
+        BSTNode <T> bin = new BSTNode<T>();
+        bin.set(in);
+        if (root == null) {
+            root = bin;
+        } else {
+            insert_re(root, bin);
+        }
+    }
+
+    private void insert_re(BSTNode parent, BSTNode insertMe) {
+        if (parent.getc().compareTo(insertMe.getc()) > 0) {
+            if (parent.getLeft() == null) {
+                parent.setLeft(insertMe);
+            } else {
+                insert_re(parent.getLeft(), insertMe);
+            }
+        } else {
+            if (parent.getRight() == null) {
+                parent.setRight(insertMe);
+            } else {
+                insert_re(parent.getRight(), insertMe);
+            }
+        }
+    }
+
+    /**
+     * in order traversal of the tree, using println to output the elements
+     */
+    public void inOrderPrint() {
+        if (root != null) {
+            inOrderPrint_re(root);
+        }
+    }
+
+    private void inOrderPrint_re(BSTNode<T> parent) {
+        if (parent == null) return;
+        inOrderPrint_re(parent.getLeft());
+        System.out.println(parent.get());
+        inOrderPrint_re(parent.getRight());
+    }
+    private void inOrderArray_re(BSTNode<T> parent, ArrayList<BSTNode> list ) {
+        if (parent == null) return;
+        inOrderArray_re(parent.getLeft(), list);
+        list.add(parent);
+        inOrderArray_re(parent.getRight(), list);
+    }
+    /**
+     * See if an element exists in the tree
+     * @param q element to search for in the tree
+     * @return true if an element in the tree is equal to the q, false otherwise
+     */
+    public boolean exists(T q) {
+        return exists_re(q,root);
+    }
+
+    private boolean exists_re(T q, BSTNode<T> parent) {
+        if (parent == null) return false;
+        if (((Comparable)q).compareTo(parent.getc()) == 0) { //Typecasting shenanigans necessary for compareTo to work
+            return true;
+        } else if (((Comparable)q).compareTo(parent.getc()) < 0) {
+            return exists_re(q, parent.getLeft());
+        } else {
+            return exists_re(q, parent.getRight());
+        }
+    }
+
+    public void printTree(){
+        Queue<BSTNode> nodes = new LinkedList<>();
+        BSTNode<T> completionNode = root;
+        BSTNode<T> curr = root;
+        nodes.add(curr);
+        while(nodes.size() > 0){
+            
+            System.out.print(curr.get()+ "\t");
+            if(curr.getLeft() != null){
+                nodes.add(curr.getLeft());
+            }
+            if(curr.getRight() != null){
+                nodes.add(curr.getRight());
+            }
+            if(curr == completionNode){
+                System.out.print("\r\n");
+                Queue<BSTNode> flipNodes = new LinkedList<>();
+                while(nodes.size() > 1){
+                    flipNodes.add(nodes.remove());
+                }
+                completionNode = nodes.peek();
+                flipNodes.add(nodes.remove());
+                while(flipNodes.size() > 0){
+                    nodes.add(flipNodes.remove());
+                }
+            }
+            
+            nodes.remove();
+            
+            curr = nodes.peek();
+        }
+
+    }
+    public void balance(){
+        BSTNode<T> parent = root;
+        ArrayList<BSTNode> list = new ArrayList<BSTNode>();
+        if (root != null) {
+            inOrderArray_re(parent, list);
+        }
+        System.out.println(list.size());
+        balance_re(parent, list);
+        
+    }
+    private void balance_re(BSTNode parent, ArrayList<BSTNode> list){
+      int median = (int)(list.size()/2.0);
+      System.out.println(median);
+        parent = list.get(median);
+        ArrayList<BSTNode> list_left = new ArrayList<BSTNode>();
+        ArrayList<BSTNode> list_right = new ArrayList<BSTNode>();
+        for(int i = 0; i<median; i++){
+            list_left.add(list.get(i));
+        }
+        for(int i = median+1; i < list.size(); i++){
+            list_right.add(list.get(i));
+        }  
+        balance_re(parent.getLeft(), list_left);
+        balance_re(parent.getRight(), list_right);
+    }
+    public class BSTNode <X>
+    {
+        X val;
+
+        BSTNode left;
+        BSTNode right;
+
+        BSTNode getLeft() {return left;}
+
+        BSTNode getRight() {return right;}
+
+        void setLeft(BSTNode bn) {left = bn;}
+
+        void setRight(BSTNode bn) {right = bn;}
+
+        X get() {return val;}
+
+        void set(X v) {val = v;}
+
+        Comparable getc() { return (Comparable) val;}
+    }
+}
---
+++ bstest.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class bstest here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class bstest
+{
+    public static void main (String [] args){
+        BST<Integer> testy = new BST<Integer>();
+        testy.insert(3);
+        testy.insert(4);
+        testy.insert(10);
+        testy.insert(15);
+        testy.insert(22);
+        testy.printTree();
+        testy.balance();
+        testy.printTree();
+    }
+}
